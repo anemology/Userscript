2e9f9d2d2847912472d2f298182e6775b14f6cdc
anemology <soarswind@gmail.com>
2018-07-22 21:49:24 +0800

    add bahamut_old_search.user.js

---
+++ bahamut_old_search.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Bahamut Old Search
+// @namespace   https://github.com/anemology/Userscript
+// @author      獨孤
+// @version     1
+// @description 巴哈舊搜尋，並可只搜尋一個字
+// @grant       none
+// @include     https://forum.gamer.com.tw/*
+// @run-at document-idle
+// ==/UserScript==
+(function () {
+    document.querySelector('#searchbox').style.display = 'none';
+    document.querySelector('#old_search_searchbox').style.display = '';
+    var SearchInput = document.getElementById('old_search_input');
+    if (SearchInput.value == '') {
+        SearchInput.value = '\\';
+    }
+})();
