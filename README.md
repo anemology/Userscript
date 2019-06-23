3dbe9b79dda96387f8c3bc4472dc694a38d2312e
anemology <soarswind@gmail.com>
2019-06-23 11:37:34 +0800

    add bahamut_disable_lazy_loading.js

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Some userscripts that I use.
 
 You need Tampermonkey or Violentmonkey to use these scripts.
 
-## Bahumut Old Search
+## Bahamut Old Search
 
 Set Bahamut Search to old mode, and you can search with one word.
 
@@ -13,3 +13,7 @@ Set Bahamut Search to old mode, and you can search with one word.
 Get the full-size profile image of Instagram.
 
 Check the web console to get the url.
+
+## Bahamut Disable Lazy Loading
+
+Disable Bahumut image lazy loading in fourms.
---
+++ bahamut_disable_lazy_loading.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Bahamut Disable Lazy Loading
+// @namespace   https://github.com/anemology/Userscript
+// @author      獨孤
+// @version     1
+// @description 停用巴哈姆特的 Lazy Loading
+// @grant       none
+// @include     https://forum.gamer.com.tw/*
+// @run-at      document-idle
+// ==/UserScript==
+(function () {
+  let allImgs = document.querySelectorAll("img.lazyload");
+  for (let i = 0; i < allImgs.length; i++) {
+    allImgs[i].classList.remove("lazyload");
+    allImgs[i].src = allImgs[i].dataset.src;
+  }
+})();
